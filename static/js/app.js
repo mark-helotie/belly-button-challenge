78061b023c08797bc01ecfff509438d12ef25bd8
Mark Helotie <43053988+mark-helotie@users.noreply.github.com>
2023-09-21 16:43:27 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // Define URL to read in data
-// const url = "./static/js/samples.json";
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = "./samples.json";
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 function Read(sample) {
     d3.json(url).then((data) => {
